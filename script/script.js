ad242e8e60ca890777b4b34a5de3643fd7960b46
Bushra <beshansaed@gmail.com>
2025-05-17 00:39:49 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -5,7 +5,6 @@ const barIcon = document.querySelector(".bar-icon");
 const heedings = document.querySelectorAll(".text-lg");
 const text = document.querySelectorAll(".text-md");
 const text1 = document.querySelectorAll(".text-sm");
-
 var isClicked = true;
 var isMatched = false;
 
@@ -15,7 +14,7 @@ function changeScreenWidth() {
     links.classList.replace("d-flex", "d-hidden");
     barIcon.classList.replace("d-hidden", "d-block");
 
-    /*hero, stats, contact, newsletter */
+    /* text */
     if (mQ1.matches) {
       isMatched = true;
       heedings.forEach((e) => e.classList.replace("text-lg", "text-sm"));
@@ -23,34 +22,29 @@ function changeScreenWidth() {
       isMatched = false;
       heedings.forEach((e) => e.classList.replace("text-lg", "text-md"));
     }
-
-    text.forEach((t) => t.classList.replace("text-md", "text-sm"));
-    text1.forEach((t) => t.classList.replace("text-sm", "text-xsm"));
+    text.forEach((e) => e.classList.replace("text-md", "text-sm"));
+    text1.forEach((e) => e.classList.replace("text-sm", "text-xsm"));
   } else {
     //block -> in case after clicking baricon
-    links.classList.remove("open");
     links.classList.replace(isClicked ? "d-hidden" : "d-block", "d-flex");
     barIcon.classList.replace("d-block", "d-hidden");
 
-    /*hero, stats, contact, newsletter */
+    /* text */
     heedings.forEach((e) => {
       e.classList.replace(isMatched ? "text-sm" : "text-md", "text-lg");
     });
-
-    text.forEach((t) => t.classList.replace("text-sm", "text-md"));
-    text1.forEach((t) => t.classList.replace("text-xsm", "text-sm"));
+    text.forEach((e) => e.classList.replace("text-sm", "text-md"));
+    text1.forEach((e) => e.classList.replace("text-xsm", "text-sm"));
   }
 }
 
 /* navbar */
 function clickBarIcon() {
   if (isClicked) {
-    links.classList.add("open");
     links.classList.replace("d-hidden", "d-block");
     isClicked = false;
   } else {
     links.classList.replace("d-block", "d-hidden");
-    links.classList.remove("open");
     isClicked = true;
   }
 }
