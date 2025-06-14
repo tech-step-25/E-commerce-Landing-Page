1a04020c270791efd88bbb2bc895b128bec69ca4
Tech Step <beshansaed@gmail.com>
2025-06-15 01:35:14 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
 const links = document.querySelector(".links");
 const barIcon = document.querySelector(".bar-icon");
+const showIcons = [];
+const values = [];
+for (i = 1; i < 5; i++) {
+  showIcons[i] = document.querySelector(`.show-icon-${i}`);
+  values[i] = document.querySelector(`.value-${i}`);
+}
 
 barIcon.addEventListener("click", () => {
   links.classList.toggle("open");
+});
+
+showIcons.forEach((element,inedx) => {
+  element.addEventListener("click", () => {
+    values[inedx].classList.toggle("d-block");
+    values[inedx].classList.toggle("d-hidden");
+  })
 });
